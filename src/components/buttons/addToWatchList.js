00812cf03c325736675c5f43fef7914d9189becf
Yannan Qian <20091632@mail.wit.ie>
2020-11-12 14:18:17 +0000

    New button for upcoming movies page

---
+++ src/components/buttons/addToWatchList.js
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+import {MoviesContext} from "../../contexts/moviesContext";
+
+const AddToWatchListButton = ({ movie }) => {
+  const context = useContext(MoviesContext);
+
+  const handleAddToWatchList = e => {
+  };
+  return (
+    <button
+      type="button"
+      className="btn w-100 btn-primary"
+      onClick={handleAddToWatchList}
+    >
+      Add to Watch List
+    </button>
+  );
+};
+
+export default AddToWatchListButton;
--- src/pages/upcomingMoviesPage.js
+++ src/pages/upcomingMoviesPage.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import StubAPI from "../api/stubAPI";
 import PageTemplate from '../components/templateMovieListPage'
 import { getUpcomingMovies } from "../api/tmdb-api";
-import AddToFavoritesButton from '../components/buttons/addToFavorites'
+import AddToWatchListButton from '../components/buttons/addToWatchList'
 
 const MovieListPage = () => {
   const [movies, setMovies] = useState([]);
@@ -12,7 +12,7 @@ const MovieListPage = () => {
     });
   }, []);
 
-  const addToFavorites = movieId => {
+  const addToWatchList = movieId => {
     setMovies(movies => {
       const index = movies.map(m => m.id).indexOf(movieId);
       StubAPI.add(movies[index]);
@@ -27,7 +27,7 @@ const MovieListPage = () => {
         title='Upcoming Movies'
         movies={movies}
         action={(movie) => {
-          return <AddToFavoritesButton movie={movie} /> 
+          return <AddToWatchListButton movie={movie} /> 
         }}
       />
   );
